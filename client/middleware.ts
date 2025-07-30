a9c9d0620667a7838185eec734902ec2bed4753e
unluckynilesh <nileshinde001@gmail.com>
2025-07-30 19:41:53 +0530

    clerk latest

--- client/middleware.ts
+++ client/middleware.ts
@@ -4,8 +4,8 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
+    "/((?!_next|static|.*\\..*).*)",
+    "/", // root route
   ],
-  runtime: 'nodejs', // THIS LINE IS MANDATORY for the fix
-}
+  runtime: 'nodejs', // Keep this line for successful deployment
+};
