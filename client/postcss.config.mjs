f12ad34190a3f6a373d493b64fcb7f68d26bd438
unluckynilesh <nileshinde001@gmail.com>
2025-07-30 20:16:40 +0530

    clerk setup

--- client/postcss.config.mjs
+++ client/postcss.config.mjs
@@ -1,9 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // This block is required for Node.js runtime in middleware on Next.js 15+
-  experimental: {
-    nodeMiddleware: true,
-  },
+const config = {
+  plugins: ["@tailwindcss/postcss"],
 };
 
-export default nextConfig;
+export default config;
